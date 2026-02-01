d741aa98c40041feb4944ab7e011a9bce5c44687
Manishsurabhi23 <149724611+Manishsurabhi23@users.noreply.github.com>
2026-02-01 00:15:50 -0500

    Add imports for data processing and prediction

--- application.py
+++ application.py
@@ -2,10 +2,12 @@ import pickle
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
 import numpy as np
+
 from sklearn.preprocessing import StandardScaler
 from src.exception import CustomException
 from src.logger import logging
 from src.pipeline.predict_pipeline import PredictPipeline, CustomData
+
 import sys
 import os
 
