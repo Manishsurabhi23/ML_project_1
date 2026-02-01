55af9cd558d821d803c88a002f9fa1374dba59c3
Manishsurabhi23 <149724611+Manishsurabhi23@users.noreply.github.com>
2026-02-01 08:45:45 -0500

    Change Flask app to run on port 8080
    Updated Flask app to run on port 8080.

--- application.py
+++ application.py
@@ -46,4 +46,5 @@ def predict_datapoint():
         raise CustomException(e, sys)
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    #app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=8080)
